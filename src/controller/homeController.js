a1724c35c4ab9b9eb96f7900a0645eacb3586408
ThachNhe <thachdinh110902@gmail.com>
2023-07-08 17:09:48 +0700

    connectDatabase

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,13 +1,31 @@
+import connection from "../configs/connectDB";
+
 let getHomePage = (req, res) => {
-    //logic
-    return res.render("index.ejs");
-}
+  //logic
+  let data = [];
+  connection.query("SELECT * FROM `users` ", function (err, results, fields) {
+    console.log("checking mysql");
+    //console.log(results);
+    results.map((row) => {
+      data.push({
+        id: row.id,
+        email: row.email,
+        firstName: row.firstName,
+        lastName: row.lastName,
+        address: row.address,
+      });
+    });
+    console.log(">> check : ", data);
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
+};
+
 
 let getAboutPage = (req, res) => {
-    return res.send("I'm Thach Nhe");
-}
+  return res.send("I'm Thach Nhe");
+};
 
 module.exports = {
-    getHomePage,
-    getAboutPage
-}
+  getHomePage,
+  getAboutPage,
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web' ;
+import connection from './configs/connectDB';
+
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8080;
+
 configViewEngine(app);
 
 initWebRoute(app);
